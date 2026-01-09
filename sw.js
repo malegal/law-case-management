9fad0c533efc7baf879fd40e04a3cd863141ee35
malegal <ma.legal.eg@outlook.com>
2026-01-09 12:36:59 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'law-case-management-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/config.js',
+  '/js/database.js',
+  '/js/cases.js',
+  '/js/sessions.js',
+  '/js/reports.js',
+  '/js/main.js',
+  '/assets/icon-192.png',
+  '/assets/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
